400b8da6033a7e2bbf11828a3802d8d92d1faf27
aei <61337085+wizardAEI@users.noreply.github.com>
2021-04-28 19:54:43 +0800

    1

--- qingshu3/index.js
+++ qingshu3/index.js
@@ -1,26 +1,74 @@
-let degree=180;
+let txt=[" 我甚至不知道你的名字，可那天的月亮🌙真美......你也是。","我知道我喜欢上你,可能只需要一秒钟<br>可是我喜欢你，可以用一辈子。","","每一次见你的时候，湖南姑娘总那么温柔。","你是我辗转反侧的梦<br>你是我坠入人间的星辰","日日月月..."];
+let txti=0;
+let degree=157;
 function sun()
 {
+    document.getElementById("cloud").style.transform="scale(2.6,2) translateX(50vw)";
     let lis=document.getElementsByClassName('box1');
     for (let i=0;i<lis.length;i++) {
         lis[i].style.transform=`rotate(${degree}deg)`;
+        degree+=200;
         }
-    let lis2=document.getElementsByClassName('box0');
-    for (let i=0;i<lis2.length;i++) {
-        lis2[i].style.backgroundColor='cornflowerblue';
-        degree+=180;
+        let lis2=document.getElementsByClassName('box0');
+        for (let i=0;i<lis2.length;i++) {
+        lis2[i].style.backgroundColor='#02475e';
         }
+    document.getElementById("star").style.opacity=1;
+    document.getElementById("star2").style.opacity=1;
+    document.getElementById("star3").style.opacity=1;
+    document.getElementById("star4").style.opacity=1;
+    document.getElementById("star5").style.opacity=1;
+    let daytxt=document.getElementById("daytxt");
+    daytxt.style.opacity=0;
+    setTimeout(function(){
+    if(txti<txt.length)
+        {
+        daytxt.innerHTML=`${txt[txti]}`;
+        txti++;
+        }
+    else
+        daytxt.innerHTML=`${txt[txti-1]}`;
+    daytxt.style.color="#28b5b5b3";
+    daytxt.style.opacity=1;
+    },1500);
+    setTimeout(function(){
+    document.getElementById("star").style.transform="translate(-100vw,100vh)";
+    document.getElementById("star2").style.transform="translate(-100vw,100vh)";
+    },2500);
 }
 //这里点击月亮，获取它转的度数，实现让他转一圈相当于回去
 function moon()
 {
+    document.getElementById("cloud").style.transform="scale(2.6,2) translateX(0vw)";
+    document.getElementById("star").style.opacity=0;
+    document.getElementById("star2").style.opacity=0;
+    document.getElementById("star3").style.opacity=0;
+    document.getElementById("star4").style.opacity=0;
+    document.getElementById("star5").style.opacity=0;
     let lis=document.getElementsByClassName('box1');
     for (let i=0;i<lis.length;i++) {
         lis[i].style.transform=`rotate(${degree}deg)`;
-        degree+=180;
+        degree+=160;
         }
-    let lis2=document.getElementsByClassName('box0');
-    for (let i=0;i<lis2.length;i++) {
-        lis2[i].style.backgroundColor='#02475e';
+        let lis2=document.getElementsByClassName('box0');
+        for (let i=0;i<lis2.length;i++) {
+            lis2[i].style.backgroundColor='#fefecce6';
         }
+        let daytxt=document.getElementById("daytxt");
+        daytxt.style.opacity=0;
+        setTimeout(function(){   
+        if(txti<txt.length)
+            {
+            daytxt.innerHTML=`${txt[txti]}`;
+            txti++;
+            }
+        else
+            daytxt.innerHTML=`${txt[txti-1]}`;
+        daytxt.style.color="#393e46b3";
+        daytxt.style.opacity=1;
+        },1500);
+        setTimeout(function(){
+            document.getElementById("star").style.transform="translate(0vw,0vh)";
+            document.getElementById("star2").style.transform="translate(0vw,0vh)";
+            },1500);
 }
