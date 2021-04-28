55fd205bce287df34b06348ca9f1bbad95b996e3
aei <61337085+wizardAEI@users.noreply.github.com>
2021-04-28 11:53:19 +0800

    1

---
+++ qingshu3/index.js
@@ -0,0 +1,26 @@
+let degree=180;
+function sun()
+{
+    let lis=document.getElementsByClassName('box1');
+    for (let i=0;i<lis.length;i++) {
+        lis[i].style.transform=`rotate(${degree}deg)`;
+        }
+    let lis2=document.getElementsByClassName('box0');
+    for (let i=0;i<lis2.length;i++) {
+        lis2[i].style.backgroundColor='cornflowerblue';
+        degree+=180;
+        }
+}
+//这里点击月亮，获取它转的度数，实现让他转一圈相当于回去
+function moon()
+{
+    let lis=document.getElementsByClassName('box1');
+    for (let i=0;i<lis.length;i++) {
+        lis[i].style.transform=`rotate(${degree}deg)`;
+        degree+=180;
+        }
+    let lis2=document.getElementsByClassName('box0');
+    for (let i=0;i<lis2.length;i++) {
+        lis2[i].style.backgroundColor='cornsilk';
+        }
+}
