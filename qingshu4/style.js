9fe98f1cba933a13638e6fc48d4290e4d9cad643
aei <61337085+wizardAEI@users.noreply.github.com>
2021-04-29 01:28:49 +0800

    1

---
+++ qingshu4/style.js
@@ -0,0 +1,9 @@
+function funclick()
+{
+    let x = document.createElement("audio");
+    x.setAttribute("src", "./video/55.mp3");
+    x.setAttribute("autoplay","autoplay");
+    let yinyan=document.getElementById("yinyan");
+    yinyan.setAttribute("style","opacity:0");
+    //yinyan.style.opacity=0;
+}
