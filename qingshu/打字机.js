5b90648990ead20acea8cc5526b932bc9e0e4ec7
aei <61337085+wizardAEI@users.noreply.github.com>
2021-04-30 23:15:16 +0800

    给范露的网页，嘘，别告诉她....
    lulululu

--- qingshu/打字机.js
+++ qingshu/打字机.js
@@ -3,8 +3,8 @@
  * author @AEI
  */
 
-let str="我愿穷极一生，做一场有你的梦......<那时我们在太空漫步，月球上相爱，然后摘下氧气头盔，接吻死去。在太阳的光线和宇宙辐射下永恒，你的双眼像猫眼星系和玫瑰星云那样，承载了我的所有.......<于是我才想起来，原来那年的春风，并不是吹散了寒冬，而是我站在你的身后，春风便把你推向了我。";//利用<作为换行符
-let strp=" ";
+let str="我愿穷极一生，做一场有你的梦......<那时我们在太空漫步，月球上相爱，然后摘下氧气头盔，接吻死去。在太阳的光线和宇宙辐射下永恒，你的双眼像猫眼星系和玫瑰星云那样，承载了我的所有.......<于是我才想起来，原来那年的春风，并不是吹散了寒冬，而是我站在你的身后，春风便把你推向了我。"//利用<作为换行符
+let strp="";
 let i=0;
 function print1()
 {
@@ -25,7 +25,7 @@ function print2()
     setTimeout(()=>{document.getElementById("box1").innerHTML=strp+' ';},100);
     setTimeout(()=>{document.getElementById("box1").innerHTML=strp+'|';},630);
 }
-setInterval(() => {
+let printid=setInterval(() => {
     print1();
     if(i==str.length)
     clearInterval(printid);
